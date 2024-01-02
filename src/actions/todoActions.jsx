5920bd2855125a82388e974d766728d8f0d53e86
Muhammad Ali Mazhar butt <ali.mazhar@devsinc.com>
2024-01-02 23:35:51 +0500

    redux project structure complete

---
+++ src/actions/todoActions.jsx
@@ -0,0 +1,11 @@
+// actions/todoActions.js
+export const addTodo = (text) => ({
+    type: 'ADD_TODO',
+    payload: { id: Date.now(), text },
+  });
+  
+  export const deleteTodo = (id) => ({
+    type: 'DELETE_TODO',
+    payload: { id },
+  });
+  
---
+++ src/reducers/todoReducer.jsx
@@ -0,0 +1,17 @@
+// reducers/todoReducer.js
+const initialState = [];
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, { id: action.payload.id, text: action.payload.text }];
+
+    case 'DELETE_TODO':
+      return state.filter((todo) => todo.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
